ef44dc5f27df1e93abeff6270041ec3cfd35093e
Toufeeq T <toufeeqt+dbt@gmail.com>
2024-02-16 06:43:16 +0000

    fix: some code changes and added new csv seed

--- models/staging/staging/stg_ext_green_tripdata.sql
+++ models/staging/staging/stg_ext_green_tripdata.sql
@@ -1,21 +1,32 @@
-with 
+{{
+  config(
+    materialized = "view"
+    )
+}}
 
-source as (
-
-    select * from {{ source('staging', 'ext_green_tripdata') }}
-
-),
-
-renamed as (
+with source as (
+    select * 
+    from {{ source('staging', 'ext_green_tripdata') }}
+    WHERE vendorid is not null
+    QUALIFY(ROW_NUMBER() OVER(PARTITION BY vendorid,lpep_pickup_datetime))=1
+)
+,renamed as (
 
     select
-        vendorid,
-        lpep_pickup_datetime,
-        lpep_dropoff_datetime,
+        {{ dbt_utils.generate_surrogate_key(['vendorid','lpep_pickup_datetime']) }} as trip_id,
+        CAST(vendorid     as INT64) as vendor_id,
+        CAST(ratecodeid   as INT64) as rate_code_id,
+        CAST(pulocationid as INT64) as pu_location_id,
+        CAST(dolocationid as INT64) as do_location_id,
+        
+        -- vendorid,    
+        -- ratecodeid,  
+        -- pulocationid,
+        -- dolocationid,
+        
+        TIMESTAMP_MICROS(CAST(lpep_pickup_datetime /1000 as INT64)) as pickup_datetime,
+        TIMESTAMP_MICROS(CAST(lpep_dropoff_datetime/1000 as INT64)) as dropoff_datetime ,
         store_and_fwd_flag,
-        ratecodeid,
-        pulocationid,
-        dolocationid,
         passenger_count,
         trip_distance,
         fare_amount,
@@ -35,4 +46,10 @@ renamed as (
 
 )
 
-select * from renamed
+SELECT * 
+FROM renamed
+
+-- dbt build --select stg_ext_green_tripdata --vars '{'is_test_run': false}'
+{% if var('is_test_run', default=true) %}
+limit 100
+{%- endif %}
